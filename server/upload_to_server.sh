ec24ff2974a9df35e8a7c57512f2078d87d4aee7
Lolle2001 <153300723+Lolle2001@users.noreply.github.com>
2024-03-06 16:35:45 +0100

    Update Server Utilities

--- server/upload_to_server.sh
+++ server/upload_to_server.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
-# scp -r include stage@131.211.54.64:~/Documents/AMPT/
+sshpass -p $1 scp -r include stage@131.211.54.64:~/Documents/AMPT/
 # scp -r src stage@131.211.54.64:~/Documents/AMPT/
-# scp -r main stage@131.211.54.64:~/Documents/AMPT/
-scp -r utils stage@131.211.54.64:~/Documents/AMPT/
-scp -r input stage@131.211.54.64:~/Documents/AMPT/
+# sshpass -p $1 scp -r main stage@131.211.54.64:~/Documents/AMPT/
+
+sshpass -p $1 scp server/Makefile stage@131.211.54.64:~/Documents/AMPT/
+
+# sshpass -p $1 scp process.job stage@131.211.54.64:~/Documents/AMPT/
+
+# sshpass -p $1 scp CMakeLists.txt stage@131.211.54.64:~/Documents/AMPT/
+ 
+# sshpass -p $1 scp compilecmake.sh stage@131.211.54.64:~/Documents/AMPT/
+# scp -r utils stage@131.211.54.64:~/Documents/AMPT/
+# scp -r input stage@131.211.54.64:~/Documents/AMPT/
 # scp ampt.job stage@131.211.54.64:~/Documents/AMPT/
 # scp rampt.sh stage@131.211.54.64:~/Documents/AMPT/
 # scp Makefile stage@131.211.54.64:~/Documents/AMPT/
-scp server/directories.sh stage@131.211.54.64:~/Documents/AMPT/utils/
-scp ampt.job stage@131.211.54.64:~/Documents/AMPT/
-scp test.job stage@131.211.54.64:~/Documents/AMPT/
+# scp server/directories.sh stage@131.211.54.64:~/Documents/AMPT/utils/
+# scp ampt.job stage@131.211.54.64:~/Documents/AMPT/
+# scp test.job stage@131.211.54.64:~/Documents/AMPT/
 
